06cf313234280e0db6b2603f11e4a749def0ec6f
rilian <dmitriyis@gmail.com>
2013-05-26 00:15:07 +0300

    initialize local crunchers database

--- config.rb
+++ config.rb
@@ -6,6 +6,17 @@ puts "Loaded Projects #{@projects.inspect}"
 
 USERS_SEARCH_QUERY = "user_search.php?action=search&search_string=&country=#{@settings['target_country']}&profile=either&team=either&search_type=total"
 
-# Set up database schema
 @db = SQLite3::Database.new('crunchers.sqlite3')
 
+# Set up database schema and perform cleanup
+@db.execute 'CREATE TABLE IF NOT EXISTS crunchers (
+  id INTEGER PRIMARY KEY,
+  user_id INTEGER,
+  name TEXT,
+  project_url TEXT,
+  team_name TEXT,
+  country TEXT,
+  credits TEXT
+);'
+@db.execute 'CREATE UNIQUE INDEX IF NOT EXISTS unique_user_id_project_url ON crunchers (user_id, project_url);'
+@db.execute 'DELETE FROM crunchers;'
